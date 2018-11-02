5c9b783b3bef05af0fce28747f3f52c4b61d34ba
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-02 18:46:55 -0300

    Changes to students and subjects

---
+++ Truffle/contracts/Students.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.19;
+
+import "./Subjects.sol";
+
+contract StudentsContract {
+
+  struct Student {
+
+  }
+
+  struct SubjectData {
+    string name;
+    
+  }
+
+}
---
+++ Truffle/contracts/Subjects.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.19;
+
+contract SubjectsContract {
+
+  struct SomeStruct {
+
+  }
+
+}
