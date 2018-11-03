0a6a85200ba8a1651cbcd5b28413d735baa33182
IvillanuevaItba <ivillanueva@itba.edu.ar>
2018-11-03 05:04:12 -0300

    Changes

--- Truffle/contracts/DecentralizedUniversity.sol
+++ Truffle/contracts/DecentralizedUniversity.sol
@@ -1,28 +1,16 @@
-contract SubjectContract {
+pragma solidity ^0.4.24;
 
-  Administrator admin;
-  function SetAdministrator(address _address){
-    admin = Administrator(_address);
+contract Structure {
+  struct University {
+    address owner;
+    uint UniversityID;
+    Subject[] subjects;
+    uint SubjectCount;
+    string name;
+    mapping (address => Student) walletToStudent;
   }
 
-	struct Subject{
-		address owner;
-		string name;
-		string contents;
-		uint SubjectID;
-		uint depth;
-		University university;
-	}
-}
-
-contract StudentContract {
-
-  Administrator admin;
-  function SetAdministrator(address _address){
-    admin = Administrator(_address);
-  }
-
-	struct Student{
+  struct Student{
 		address owner;
 		string name;
 		University university;
@@ -30,70 +18,98 @@ contract StudentContract {
     	SubjectData[] currentSubjects;
 	}
 
-	struct SubjectData{
+  struct SubjectData{
 		string name;
 		University university;
 		uint grade;
 	}
-	/*
-		Creates a new student and adds it to the university array of students
-	*/
-	function CreateNewStudent(address _address, string _name, uint _universityId) external {
-		require(allUniversities[_universityId] != 0);
-		require(msg.sender == allUniversities[_universityId].owner);
 
-		allUniversities[_universityId].students.push(Student(_address, _name, allUniversities[_universityId], new SubjectData[], new SubjectData[]));
-	}
-	/*
-		Creates a new subjectData and adds it to the student
-	*/
-	function CreateNewSubjectData(address _studentAddress, uint _subjectId, uint _universityId) external {
-		require(allUniversities[_universityId] != 0);
-		require(msg.sender == allUniversities[_universityId].owner);
-		require(allUniversities[_universityId].walletToStudent[_studentAddress] != 0);
-
-		University auxUniversity = allUniversities[_universityId];
-		Subject auxSubject = auxUniversity.subjects[_subjectId];
-		auxUniversity.walletToStudent[_studentAddress].currentSubjects.push(SubjectData(auxSubject.name, auxUniversity, 0));
-	}
+  struct Subject{
+    address owner;
+    string name;
+    string contents;
+    uint SubjectID;
+    uint depth;
+    University university;
+  }
 }
 
-contract UniversityContract {
+contract ContractsInterface {
+  function SetAdministrator(address _address);
+}
+
+contract Administrator is Structure{
+
+  University[] allUniversities;
+  uint universityCount = 0;
+
+  ContractsInterface universityContract;
+  ContractsInterface studentContract;
+  ContractsInterface subjectContract;
+
+  function getUniversity() view public {
+
+  }
+
+  function SetSubjectContract(address _address) {
+    subjectContract = ContractsInterface(_address);
+  }
+  function SetStudentContract(address _address) {
+    studentContract = ContractsInterface(_address);
+  }
+  function SetUniversityContract(address _address) {
+    universityContract = ContractsInterface(_address);
+  }
+  function SetAdministrator(){
+    universityContract.SetAdministrator(this);
+    studentContract.SetAdministrator(this);
+    subjectContract.SetAdministrator(this);
+  }
+
+  function CreateUniversity(address _owner, string _name) external {
+  	allUniversities[universityCount] = University(_owner, universityCount,  Subject[], 0, _name, 0);
+  	universityCount++;
+  }
+}
+
+contract UniversityContract is Structure {
+
+  Administrator admin;
+  function SetAdministrator(address _address){
+    admin = Administrator(_address);
+  }
+}
+
+contract StudentContract is Structure{
 
   Administrator admin;
   function SetAdministrator(address _address){
     admin = Administrator(_address);
   }
 
+	/*
+		Creates a new student and adds it to the university array of students
+	*/
+	function CreateNewStudent(address _address, string _name, uint _universityId) external {
+		require(admin.allUniversities[_universityId] != 0);
+		require(msg.sender == admin.allUniversities[_universityId].owner);
+
+		admin.allUniversities[_universityId].students.push(Student(_address, _name, admin.allUniversities[_universityId], new SubjectData[], new SubjectData[]));
+	}
+	/*
+		Creates a new subjectData and adds it to the student
+	*/
+	function CreateNewSubjectData(address _studentAddress, uint _subjectId, uint _universityId) external {
+		require(admin.allUniversities[_universityId] != 0);
+		require(msg.sender == admin.allUniversities[_universityId].owner);
+		require(admin.allUniversities[_universityId].walletToStudent[_studentAddress] != 0);
+
+		University auxUniversity = admin.allUniversities[_universityId];
+		Subject auxSubject = auxUniversity.subjects[_subjectId];
+		auxUniversity.walletToStudent[_studentAddress].currentSubjects.push(SubjectData(auxSubject.name, auxUniversity, 0));
+	}
 }
 
+contract SubjectContract is Structure{
 
-contract AdministratorContract {
-
-  University[] allUniversities;
-  uint universityCount = 0;
-
-  University UniversityContract;
-  Student StudentContract;
-  Subject SubjectContract;
-
-  function SetSubjectContract(address _address) {
-    SubjectContract = Subject(_address);
-  }
-  function SetStudentContract(address _address) {
-    StudentContract = Student(_address);
-  }
-  function SetUniversityContract(address _address) {
-    UniversityContract = University(_address);
-  }
-  function SetAdministrator(){
-    UniversityContract.SetAdministrator(this);
-    StudentContract.SetAdministrator(this);
-    SubjectContract.SetAdministrator(this);
-  }
-
-  function CreateUniversity(address _owner, string _name) external {
-  	//allUniversities[universityCount] = University(_owner, universityCount, new Subject[], 0, _name, new mapping (address => Student));
-  	//universityCount++;
-  }
 }
