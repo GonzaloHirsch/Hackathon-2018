df31db1d64adccfd57b2f028fd0e4d63cb493ec1
IvillanuevaItba <ivillanueva@itba.edu.ar>
2018-11-02 23:17:53 -0300

    Create Administrator, Ownable

--- Truffle/contracts/University.sol
+++ Truffle/contracts/University.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.19
 
-Subject subjects = require("Subject.sol")
+import "./Subjects.sol";
+import "./Students.sol";
 
 contract UniversityContract {
   struct University {
-    address Owner;
+    address owner;
+    Subject[] subjects;
+    string name;
+    Student[] students;
   }
+
+function createSubject(string name, University university, string contents, uint16 depth, address subjectOwner) {
+  subjects.push(new Subject())
+}
 
 
 }
