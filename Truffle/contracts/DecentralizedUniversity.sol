8a1d7044980ee301abb0559f6de6a24f3fdc9c58
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-03 04:22:05 -0300

    Update Truffle/contracts/DecentralizedUniversity.sol

--- Truffle/contracts/DecentralizedUniversity.sol
+++ Truffle/contracts/DecentralizedUniversity.sol
@@ -29,8 +29,26 @@ contract Student {
 		University university;
 		uint grade;
 	}
+	/*
+		Creates a new student and adds it to the university array of students
+	*/
 	function CreateNewStudent(address _address, string _name, uint _universityId) external {
-		
+		require(allUniversities[_universityId] != 0);
+		require(msg.sender == allUniversities[_universityId].owner);
+
+		allUniversities[_universityId].students.push(Student(_address, _name, allUniversities[_universityId], new SubjectData[], new SubjectData[]));
+	}
+	/*
+		Creates a new subjectData and adds it to the student
+	*/
+	function CreateNewSubjectData(address _studentAddress, uint _subjectId, uint _universityId) external {
+		require(allUniversities[_universityId] != 0);
+		require(msg.sender == allUniversities[_universityId].owner);
+		require(allUniversities[_universityId].walletToStudent[_studentAddress] != 0);
+
+		University auxUniversity = allUniversities[_universityId];
+		Subject auxSubject = auxUniversity.subjects[_subjectId];
+		auxUniversity.walletToStudent[_studentAddress].currentSubjects.push(SubjectData(auxSubject.name, auxUniversity, 0));
 	}
 }
 
@@ -42,7 +60,7 @@ contract University {
 
 }
 
-contract AdministratorContract is Ownable {
+contract AdministratorContract {
 
   University[] allUniversities;
   uint universityCount = 0;
