d02801955ca8978a7da9f9787aa421256b13aa70
IvillanuevaItba <ivillanueva@itba.edu.ar>
2018-11-03 03:29:24 -0300

    Tests

--- Truffle/contracts/DecentralizedUniversity.sol
+++ Truffle/contracts/DecentralizedUniversity.sol
@@ -1,3 +1,15 @@
 contract DecentralizedUniversity {
-  
+
+  function testDe(address _address) public {
+    University(_address).test();
+  }
+
+}
+
+contract University {
+
+  function test() pure public returns(uint) {
+    return 1;
+  }
+
 }
--- Truffle/migrations/2_deploy_contracts.js
+++ Truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var DecentralizedUniversity = artifacts.require("DecentralizedUniversity");
+var University = artifacts.require("University");
 
 module.exports = function(deployer) {
   deployer.deploy(DecentralizedUniversity);
+  deployer.deploy(University);
 };
