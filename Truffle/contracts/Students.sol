36c267580387189ab9a55fd5893332b245ca692a
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-02 22:19:10 -0300

    Changed subjects

--- Truffle/contracts/Students.sol
+++ Truffle/contracts/Students.sol
@@ -1,16 +1,22 @@
 pragma solidity ^0.4.19;
 
 import "./Subjects.sol";
+import "./University.sol";
 
 contract StudentsContract {
 
   struct Student {
-
+    address owner;
+    string name;
+    SubjectData[] attendedSubjects;
+    SubjectData[] currentSubjects;
+    University university;
   }
 
   struct SubjectData {
     string name;
-    
+    University university;
+    uint8 grade;
   }
 
 }
--- Truffle/contracts/Subjects.sol
+++ Truffle/contracts/Subjects.sol
@@ -2,7 +2,16 @@ pragma solidity ^0.4.19;
 
 contract SubjectsContract {
 
-  struct SomeStruct {
+  struct Subject {
+    string name;
+    University university;
+    string contents;
+    Subject[] equivalentSubjects;
+    uint16 depth;
+    address owner
+  }
+
+  function CreateNewSubject(string name, University university, string contents, uint16 depth, address subjectOwner) returns (Subject) {
 
   }
 
