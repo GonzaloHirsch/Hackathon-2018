f5e8d6022f2e2fe990ed34c20ec162042cab98bc
IvillanuevaItba <ivillanueva@itba.edu.ar>
2018-11-03 04:05:29 -0300

    Administrator Contracts

--- Truffle/contracts/DecentralizedUniversity.sol
+++ Truffle/contracts/DecentralizedUniversity.sol
@@ -12,3 +12,28 @@ contract University {
   }
 
 }
+
+contract AdministratorContract is Ownable {
+
+  University[] allUniversities;
+  uint universityCount = 0;
+
+  University UniversityContract;
+  Student StudentContract;
+  Subject SubjectContract;
+
+  function SetSubjectContract(address _address) {
+    SubjectContract = Subject(_address);
+  }
+  function SetStudentContract(address _address) {
+    StudentContract = Student(_address);
+  }
+  function SetUniversityContract(address _address) {
+    UniversityContract = University(_address);
+  }
+
+  function CreateUniversity(address _owner, string _name) external {
+  	allUniversities[universityCount] = University(_owner, universityCount, new Subject[], 0, _name, new mapping (address => Student));
+  	universityCount++;
+  }
+}
