a873f52a6348b60d87d7769ba4b732ea115551ef
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-03 09:25:18 -0300

    Added functions to finish subjects

--- Truffle/contracts/DecentralizedUniversity.sol
+++ Truffle/contracts/DecentralizedUniversity.sol
@@ -7,7 +7,34 @@ contract Student{
 		string name;
 	}
 
+	struct FinishedCourse{
+		Course endCourse;
+		uint grade;
+	}
+
 	Course[] courses;
+	FinishedCourse[] finishedCourses;
+
+	//EXTRA FUNCTIONS
+
+	function ResetCourses() external{
+		for (uint i = 0; i<=courses.length-1; i++){
+            delete courses[i];
+            courses.length--;
+        }
+	}
+
+	function RemoveCourse(uint index) internal {
+		require(index < courses.length);
+
+        for (uint i = index; i<courses.length-1; i++){
+            courses[i] = courses[i+1];
+        }
+        delete courses[courses.length-1];
+        courses.length--;
+    }
+
+	//ADDING COURSES
 
 	function SubscribeToCourse(string _name, string _university) external{
 		courses.push(Course(_name, _university));
@@ -17,8 +44,27 @@ contract Student{
 		return courses.length;
 	}
 
-	function GetElements(uint index) view external returns(string, string){
-		Course storage aux = courses[index];
-		return (aux.university, aux.name);
+	function GetCourse(uint _index) view external returns(string, string){
+		//require(_index < courses.length);
+		Course storage aux = courses[_index];
+		return (aux.name, aux.university);
+	}
+
+	//FINISHING COURSES
+
+	function GetFinishedCoursesLength() view external returns(uint){
+		return finishedCourses.length;
+	}
+
+	function GetFinishedCourse(uint _index) view external returns(string, string, uint){
+		FinishedCourse storage aux = finishedCourses[_index];
+		Course storage auxCourse = aux.endCourse;
+		return (auxCourse.name, auxCourse.university, aux.grade);
+	}
+
+	function FinishCourse(uint _index, uint _grade) external{
+		Course storage aux = courses[_index];
+		RemoveCourse(_index);
+		finishedCourses.push(FinishedCourse(aux, _grade));
 	}
 }
