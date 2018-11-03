cb85908ca7be240a85947fdccc7cabb757f9afa9
GonzaloHirsch <hirschgonzalo@yahoo.com.ar>
2018-11-03 02:21:06 -0300

    Created University

---
+++ Truffle/contracts/University.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.23;
+
+//import "./Administrator.sol";
+
+contract UniversityContract{
+
+	struct University{
+		address owner;
+		uint universityId;
+		uint subjectCount;
+		string name;
+		//Subject array;
+		//Student Map;
+	}
+
+}
